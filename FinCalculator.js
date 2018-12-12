f1befc2ff8de27c7ed5b4a7b8025aeda8cf54606
bharathb04 <38512697+bharathb04@users.noreply.github.com>
2018-12-12 22:20:22 +1100

    Update FinCalculator.js

--- FinCalculator.js
+++ FinCalculator.js
@@ -1,8 +1,9 @@
 function finOutput() {
-  
   var n1 = document.getElementByName("finGrossIncome");
   var n2 = document.getElementByName("finRentalIncome");
   var n3 = document.getElementByName("finChildren");
+  
+  alert("Hi " + n1 + n2 + n3)
   
 If(isNaN(n1) && isNAN(n2) && isNAN(n3)){
      alert("One or more of the input fields is/are not numeric, please enter a valid value");
