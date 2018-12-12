8d98ea3bc5f2c4ac20981f5aa09ce7d371dc794e
bharathb04 <38512697+bharathb04@users.noreply.github.com>
2018-12-12 20:47:08 +1100

    Create FinCalculator.js

---
+++ FinCalculator.js
@@ -0,0 +1,11 @@
+function finOutput() {
+  var n1 = document.getElementByName("finGrossIncome");
+  
+  If(isNaN(n1)){
+     alert("The value of Gross Income has to be numeric, please enter a valid value");
+     return false;
+  }else{
+     alert("It's fine");
+     return true;
+  }
+  
